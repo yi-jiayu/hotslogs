8ed56f8a33de35667678bcecf9116ad0adc8ad44
Jiayu Yi <yi-jiayu@users.noreply.github.com>
2017-07-29 15:06:26 +0800

    Initial commit

---
+++ cmd/upload.go
@@ -0,0 +1,124 @@
+// Copyright © 2017 Jiayu Yi <yi-jiayu@users.noreply.github.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"github.com/yi-jiayu/hotslogs/hotslogs"
+)
+
+// uploadCmd represents the upload command
+var uploadCmd = &cobra.Command{
+	Use:   "upload",
+	Aliases: []string{"up"},
+	Short: "Upload new replays",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		replayDir := viper.GetString("replayDir")
+		if replayDir == "" {
+			log.Fatal("err: replay dir not set")
+		}
+
+		lastUploadTime := viper.GetString("lastUploadTime")
+		fmt.Printf("Looking for new replays since: %s\n", lastUploadTime)
+
+		since, err := time.Parse(time.RFC3339, lastUploadTime)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		now := time.Now()
+		newReplays, err := hotslogs.ListNewReplays(replayDir, since)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if len(newReplays) == 0 {
+			fmt.Println("No new replays since last upload, exiting.")
+			os.Exit(0)
+		} else {
+			fmt.Printf("Found %d new replays since last upload.\n", len(newReplays))
+		}
+
+		fmt.Println("Uploading new replays...")
+
+		sess := session.Must(session.NewSession(&aws.Config{
+			Credentials: hotslogs.StaticCredentials,
+			Region:      aws.String(hotslogs.S3BucketRegion),
+		}))
+		uploader := s3manager.NewUploader(sess)
+
+		for _, replay := range newReplays {
+			fmt.Printf("  %s: ", replay.Name())
+			result, err := hotslogs.UploadReplay(uploader, path.Join(replayDir, replay.Name()))
+			if err != nil {
+				fmt.Printf("ERROR (%s)\n", err)
+			} else {
+				fmt.Printf("OK (%s)\n", result)
+			}
+		}
+
+		// update config file
+		fmt.Print("Updating config file... ")
+
+		config := fmt.Sprintf("replayDir: %s\nlastUploadTime: %s\n", replayDir, now.Format(time.RFC3339))
+		file, err := os.OpenFile(viper.ConfigFileUsed(), os.O_WRONLY|os.O_CREATE, 0666)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		_, err = file.WriteString(config)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println("Done.")
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(uploadCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// uploadCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// uploadCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ hotslogs/upload.go
@@ -0,0 +1,147 @@
+package hotslogs
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/satori/go.uuid"
+)
+
+const (
+	AccessKeyID     = "AKIAIESBHEUH4KAAG4UA"
+	SecretAccessKey = "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"
+	S3BucketName    = "heroesreplays"
+	S3BucketRegion  = "us-west-2"
+
+	UploadEndpoint = "https://www.hotslogs.com/UploadFile?Source="
+)
+
+var (
+	StaticCredentials = credentials.NewStaticCredentials(AccessKeyID, SecretAccessKey, "")
+)
+
+func ListNewReplays(replayDir string, since time.Time) ([]os.FileInfo, error) {
+	files, err := ioutil.ReadDir(replayDir)
+	if err != nil {
+		return nil, err
+	}
+
+	newReplays := make([]os.FileInfo, 0)
+	for _, file := range files {
+		if file.ModTime().After(since) {
+			newReplays = append(newReplays, file)
+		}
+	}
+
+	return newReplays, nil
+}
+
+func UploadReplayToS3(uploader *s3manager.Uploader, key string, file io.Reader) error {
+	params := &s3manager.UploadInput{
+		Bucket: aws.String(S3BucketName),
+		Key:    &key,
+		Body:   file,
+	}
+
+	_, err := uploader.Upload(params)
+	return err
+}
+
+func GetUploadResult(key string) (string, error) {
+	URL := UploadEndpoint + "&FileName=" + key
+	resp, err := http.Get(URL)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	result, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(result), nil
+}
+
+func UploadReplay(uploader *s3manager.Uploader, path string) (string, error) {
+	UUID := uuid.NewV4()
+	key := fmt.Sprintf("%s.StormReplay", UUID)
+
+	file, err := os.Open(path)
+	if err != nil {
+		return "", err
+	}
+
+	err = UploadReplayToS3(uploader, key, file)
+	if err != nil {
+		return "", err
+	}
+
+	result, err := GetUploadResult(key)
+	return result, err
+}
+
+func UploadReplays(uploader *s3manager.Uploader, paths []string) ([]string, []error) {
+	// todo: parallelise
+	results := make([]string, len(paths))
+	errors := make([]error, len(paths))
+	for i, path := range paths {
+		result, err := UploadReplay(uploader, path)
+		if err != nil {
+			errors[i] = err
+		} else {
+			results[i] = result
+		}
+	}
+
+	return results, errors
+}
+
+func UploadNewReplays(replayDir string, since time.Time) (map[string]int, error) {
+	newReplays, err := ListNewReplays(replayDir, since)
+	if err != nil {
+		return nil, err
+	}
+
+	paths := make([]string, 0)
+	for _, replay := range newReplays {
+		p := path.Join(replayDir, replay.Name())
+		paths = append(paths, p)
+	}
+
+	sess := session.Must(session.NewSession(&aws.Config{
+		Credentials: StaticCredentials,
+		Region:      aws.String(S3BucketRegion),
+	}))
+	uploader := s3manager.NewUploader(sess)
+
+	resultsMap := make(map[string]int)
+	results, errors := UploadReplays(uploader, paths)
+	for i, err := range errors {
+		if err != nil {
+			if _, exists := resultsMap[err.Error()]; exists {
+				resultsMap[err.Error()]++
+			} else {
+				resultsMap[err.Error()] = 1
+			}
+		} else {
+			result := results[i]
+			if _, exists := resultsMap[result]; exists {
+				resultsMap[result]++
+			} else {
+				resultsMap[result] = 1
+			}
+		}
+	}
+
+	return resultsMap, nil
+}
